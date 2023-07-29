e0715d68e7e5070a629ba0bcb1f627e3cdc82da5
lucashnss <hns.lucass@gmail.com>
2023-07-28 23:44:24 -0300

    adição dos sign extends

---
+++ SignExtend16to32.v
@@ -0,0 +1,8 @@
+module SignExtend16to32(
+    input wire [15:0]in,
+    output wire [31:0]out
+);
+
+    assign out = {{16{in[15]}},in};
+
+endmodule
---
+++ SignExtend8to32.v
@@ -0,0 +1,8 @@
+module SignExtend8to32(
+    input wire[7:0] in;
+    output wire[31:0] out;
+);
+
+assign out = {{24{in[7]}},in};
+
+endmodule
